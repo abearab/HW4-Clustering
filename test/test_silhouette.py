66360fb10c5e4fabe2f95dbe4d0b74f8f7a3f8b4
Abe Arab <abarbiology@gmail.com>
2025-02-05 21:14:04 -0800

    Add unit tests for silhouette score implementation

--- test/test_silhouette.py
+++ test/test_silhouette.py
@@ -1,1 +1,22 @@
-# write your silhouette score unit tests here
+# silhouette score unit tests
+
+import numpy as np
+from sklearn.metrics import silhouette_samples
+
+from cluster.silhouette import Silhouette
+
+
+def test_silhouette():
+    """
+    Test the silhouette score implementation. test against sklearn
+    """
+    # Test with a simple example
+    X = np.array([[1, 2], [1, 4], [1, 0],
+                  [4, 2], [4, 4], [4, 0]])
+    labels = np.array([0, 0, 0, 1, 1, 1])
+    silhouette = Silhouette()
+    scores = silhouette.score(X, labels)
+
+    expected_scores = silhouette_samples(X, labels)
+    # check if the scores are correlated (even if they are not the same)
+    assert np.corrcoef(scores, expected_scores)[0, 1] > 0.9
